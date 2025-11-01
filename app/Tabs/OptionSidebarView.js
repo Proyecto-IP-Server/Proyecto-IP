76c59e68c031016ef06fe71ae1db4e821ed81bf0
cesarrodriguez5301 <cesar.rodriguez5301@alumnos.udg.mx>
2025-11-01 16:33:12 -0600

    se añadio el boton de limpiar

--- app/Tabs/OptionSidebarView.js
+++ app/Tabs/OptionSidebarView.js
@@ -16,13 +16,21 @@ export default function OptionSidebarView() {
   ]
 
   return (
-    <View style={{height:'100%', width:'100%', padding:10,
+    <View style={{height:'100%', 
+        width:'100%', 
+        padding:10, 
+        borderBlockColor: 'black', 
+        borderWidth: 1,
         borderBlockColor: 'black',
         borderWidth: 1,
         }}>
 
         {/* Muesta el encabesado */}
-        <View style={{width:'100%', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingBottom: 5}}>
+        <View style={{width:'100%', 
+          flexDirection: 'row', 
+          justifyContent: 'space-between', 
+          alignItems: 'center', 
+          paddingBottom: 5}}>
           <TextTitle>Condiciones</TextTitle>
           <Pressable>
             <Image source={{uri: 'https://cdn-icons-png.flaticon.com/512/58/58282.png'}} style={{width: 24, height: 24}} />
@@ -35,6 +43,23 @@ export default function OptionSidebarView() {
         <View>
           <TextInput placeholder='Escribe aqui...' />
           <Button title='Agregar' /> {/* El action del boton es el que va a cambiar dependiendo la de condicion escojida */}
+        </View>
+
+        <View>
+          <TextInput placeholder='Escribe aqui...' />
+          <Button title='Maestro' /> {/* El action del boton es el que va a cambiar dependiendo la de condicion escojida */}
+        </View>
+
+        {/* Botón Limpiar en la parte inferior */}
+        <View style={{ flex: 1, justifyContent: 'flex-end', marginBottom: 0 }}>
+            <Pressable style={{ 
+                backgroundColor: '#007AFF', // Color azul
+                padding: 10,
+                borderRadius: 5,
+                alignItems: 'center'
+                }}>
+                <Text style={{ color: 'white', fontWeight: 'bold' }}>Limpiar</Text>
+            </Pressable>
         </View>
 
 
